74f5c3b2bff8c78eaabc1bcc16fea278668915c0
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-05-12 16:22:57 +0200

    chore: initial commit

--- Tests/NowPlayingKitTests/NowPlayingKitTests.swift
+++ Tests/NowPlayingKitTests/NowPlayingKitTests.swift
@@ -1,6 +1,22 @@
+//
+//  NowPlayingKitTests.swift
+//  NowPlayingKitTests
+//
+//  Created by Adrian Castro on 8/5/25.
+//
+
 import Testing
 @testable import NowPlayingKit
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct NowPlayingKitTests {
+    @Test func testNowPlayingManager() async throws {
+        let manager = NowPlayingManager.shared
+        do {
+            let playbackData = try await manager.getCurrentPlayback()
+            #expect(playbackData.title.isEmpty == false)
+            #expect(playbackData.duration >= 0)
+        } catch NowPlayingError.noCurrentEntry {
+            print("No music playing")
+        }
+    }
 }
