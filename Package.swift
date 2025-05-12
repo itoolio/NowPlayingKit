7cde643f90ffef1da490e4d8fbe163aad40e257e
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-05-12 20:46:06 +0200

    chore: set platforms

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "NowPlayingKit",
+	platforms: [
+		.iOS(.v17),
+		.macOS(.v14),
+	],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NowPlayingKit",
             targets: ["NowPlayingKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NowPlayingKit"),
         .testTarget(
